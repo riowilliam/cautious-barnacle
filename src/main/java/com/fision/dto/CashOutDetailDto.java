d5af1979b4cf7263daacc994571ecc40b13e553a
Rio William <riowilliamsimamora@gmail.com>
2024-10-07 22:35:07 +0700

    add bank account name

--- src/main/java/com/fision/dto/CashOutDetailDto.java
+++ src/main/java/com/fision/dto/CashOutDetailDto.java
@@ -13,17 +13,19 @@ public class CashOutDetailDto {
     private String invoice;
     private String projectName;
     private String bankAccount;
+    private String bankAccountName;
     private String bankName;
     private BigDecimal amount;
     private BigDecimal transferFee;
     private BigDecimal totalAmount;
 
-    public CashOutDetailDto(Long idTmpCashOut, String vendorName, String invoice, String projectName, String bankAccount, String bankName, BigDecimal amount, BigDecimal transferFee, BigDecimal totalAmount) {
+    public CashOutDetailDto(Long idTmpCashOut, String vendorName, String invoice, String projectName, String bankAccount, String bankAccountName, String bankName, BigDecimal amount, BigDecimal transferFee, BigDecimal totalAmount) {
         this.idTmpCashOut = idTmpCashOut;
         this.vendorName = vendorName;
         this.invoice = invoice;
         this.projectName = projectName;
         this.bankAccount = bankAccount;
+        this.bankAccountName = bankAccountName;
         this.bankName = bankName;
         this.amount = amount;
         this.transferFee = transferFee;
