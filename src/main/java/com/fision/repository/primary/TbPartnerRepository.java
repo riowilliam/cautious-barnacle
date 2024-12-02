4b138e3fe6298b0726709b683069e8a85d0e0830
Gilang <ramadhan.gilang@anteraja.id>
2024-12-02 19:24:21 +0700

    Phase 5 : Enhance Partner

---
+++ src/main/java/com/fision/repository/primary/TbPartnerRepository.java
@@ -0,0 +1,51 @@
+package com.fision.repository.primary;
+
+import com.fision.dto.PartnerListDto;
+import com.fision.entity.primary.TbPartner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author LordDev
+ */
+@Repository
+public interface TbPartnerRepository extends JpaRepository<TbPartner, Long> {
+    @Query("SELECT new com.fision.dto.PartnerListDto (" +
+            "pt.partnerId, " +
+            "pt.partnerName, " +
+            "pt.isPpnWapu, " +
+            "pt.activeProject, " +
+            "pt.createdTm, " +
+            "pt.createdBy, " +
+            "pt.modifiedTm, " +
+            "pt.modifiedBy) " +
+            "FROM TbPartner pt " +
+            "WHERE (:partnerName IS NULL OR pt.partnerName LIKE %:partnerName%) " +
+            "AND (:ppnWapu IS NULL OR pt.isPpnWapu = :ppnWapu) " +
+            "AND (:startDate is null OR pt.createdTm >= :startDate) " +
+            "AND (:endDate is null OR pt.createdTm <= :endDate) ")
+    Page<PartnerListDto> getPartnerListPaging(@Param("partnerName") String partnerName,
+                                              @Param("ppnWapu") Integer ppnWapu,
+                                              @Param("startDate") Date startDate,
+                                              @Param("endDate") Date endDate,
+                                              Pageable pageable);
+
+    @Query("SELECT new map(pt.partnerId as partnerId, pt.partnerName as partnerName, " +
+            "CASE WHEN pt.isPpnWapu = 1 THEN true ELSE false END as ppnWapu," +
+            "pt.ppnValue as ppnValue, " +
+            "pt.activeProject as activeProject) " +
+            "FROM TbPartner pt " +
+            "WHERE (:partnerName IS NULL OR pt.partnerName LIKE %:partnerName%) ")
+    List<Map<String, Object>> getPartnerList(@Param("partnerName") String partnerName);
+
+    TbPartner findByPartnerName(String partnerName);
+    TbPartner findByPartnerId(Long partnerId);
+}
