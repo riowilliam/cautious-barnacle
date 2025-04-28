dc05056e612487077eb0118339afb7fce618832a
LordGils <ramadhangilangpermadi@gmail.com>
2025-04-28 22:35:41 +0700

    Fixing 20250428

--- src/main/java/com/fision/controller/BankController.java
+++ src/main/java/com/fision/controller/BankController.java
@@ -11,6 +11,10 @@ import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,13 +25,32 @@ import java.util.Map;
  * @author LordDev
  */
 @RestController
-@RequestMapping("/api/cashIn/")
+@RequestMapping("/api/bank/")
 @CrossOrigin
 public class BankController {
     private static final Logger logger = LoggerFactory.getLogger(BankController.class);
 
     @Autowired
     MsBankService msBankService;
+
+    @GetMapping("getBankListPaging")
+    public ResponseDto<?> getItemListPaging(@RequestParam(defaultValue = "0") int pageNo,
+                                            @RequestParam(defaultValue = "10") int pageSize,
+                                            @RequestParam(defaultValue = "createdTm") String sortBy,
+                                            @RequestParam(defaultValue = "desc") String sortOrder,
+                                            @RequestParam(required = false) String bankName) {
+        try {
+            Pageable pageable = PageRequest.of(pageNo, pageSize,
+                    sortOrder.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending());
+
+            Page<MsBank> msBankList = msBankService.getBankListPaging(bankName != null && !bankName.isEmpty() ? bankName : null, pageable);
+
+            return new ResponseDto<>(ConstantsUtils.SUCCESS, msBankList, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.info(e.getMessage(), e);
+            return new ResponseDto<>(ConstantsUtils.ERROR_SYSTEM, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @PostMapping("createBank")
     public ResponseDto<?> createBank(@RequestParam String username, @RequestBody String requestDto) {
--- src/main/java/com/fision/repository/primary/MsBankRepository.java
+++ src/main/java/com/fision/repository/primary/MsBankRepository.java
@@ -1,8 +1,12 @@
 package com.fision.repository.primary;
 
 import com.fision.entity.primary.MsBank;
+import com.fision.entity.primary.MsItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +20,9 @@ public interface MsBankRepository extends JpaRepository<MsBank, Long> {
             "OR (:bankName IS NULL OR mb.bankName LIKE %:bankName%) ")
     List<Map<String, Object>> getBankList(String bankShortName, String bankName);
     MsBank findByBankName(String bankName);
+
+    @Query("SELECT mb FROM MsBank mb " +
+            "WHERE (:bankName IS NULL OR mi.bankName LIKE %:bankName%) ")
+    Page<MsBank> getMsBankListPaging(@Param("bankName") String bankName,
+                                     Pageable pageable);
 }
